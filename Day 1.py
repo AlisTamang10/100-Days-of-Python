be5f7fd71ee9547f019f5c3a2d8140fca7b72e03
Alis Tamang <142369520+AlisTamang10@users.noreply.github.com>
2024-05-12 21:13:18 +0545

    DAY of Python
    Basic Knowledge about python.

---
+++ Day 1.py
@@ -0,0 +1,10 @@
+# Introduction  Python
+
+# dynamic type and general purpose programming supports oop
+# interpreted language and high level programming language 
+# easy to learn and write
+# open source and debugging is very easy
+# supports many libraries like selinium, opencv , nuumy , pandas etc
+print("Hello World")
+print(1+2)
+
