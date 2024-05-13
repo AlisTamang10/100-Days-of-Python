2c0c1298107a9cd1ab50ca9988e8eb8c8b6bbb97
Alis Tamang <142369520+AlisTamang10@users.noreply.github.com>
2024-05-13 17:16:56 +0545

    Day 2 'Modules and PIP CMD '

---
+++ Day2.py
@@ -0,0 +1,10 @@
+#Modules and pip in python 
+''' 
+types of modeules in python
+1: Built in Modules: These modules are already installed and we can use any time 
+2: External Module: These modules are created by someone and to use we should install packages using pip or conda
+like
+pip install pandas 
+'''
+import pandas
+
