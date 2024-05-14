5baab67f4c426526e773142f10b2c52c3cabdd8d
Alis Tamang <142369520+AlisTamang10@users.noreply.github.com>
2024-05-14 21:23:00 +0545

    Day 3
    Comments, Escape and Print Fucntion

---
+++ Day3.py
@@ -0,0 +1,24 @@
+# writing first program
+#Using print Function
+print("Hello World")
+print(5,5)
+print('Valid')
+
+#Comments, Escape Sequence and Print Statments
+# print("hello
+#       everyone ") 
+# throws an error
+
+#using \n which is escape sequence character
+
+print("Heloo \n everyone")
+
+#this is single comments
+
+'''this is multple line comments
+you know
+'''
+
+#Escape sequence Characters
+print("Heloo \"wassup \" everyone")
+print("Hey", 1, 2, sep = "-" , end = "001")
