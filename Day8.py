968800c826aba6325368016457a394909c853471
Alis Tamang <142369520+AlisTamang10@users.noreply.github.com>
2024-05-19 21:02:48 +0545

    Day8 - Strings

---
+++ Day8.py
@@ -0,0 +1,25 @@
+# Strings
+"""
+In python anything enclose between sigle or double quotation mark is considerd as string.
+String is essentially sequence or arrays of textual data.    
+"""
+# Example
+string = 'Hello Its alis '
+string1 = " Sup "
+print(string,string1)
+
+# Multi Line strings
+#Example 
+a = '''
+In the heart of the bustling city, nestled between towering skyscrapers, there was a quaint little bookstore called "Whispers of the Past." 
+The store was owned by an elderly man named Mr. Thompson, whose silver hair and kind eyes spoke of a thousand untold stories. 
+'''
+print(type(a),a)
+
+# Accessing Charecters of string
+print(string[0],string[1],string[2])
+
+#Looping through the strings
+
+for chars in string:
+    print(chars)
