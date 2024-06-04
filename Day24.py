257a21ec2ce4751c1ffaf823e7457d6509048e0f
Alis Tamang <142369520+AlisTamang10@users.noreply.github.com>
2024-06-04 22:06:22 +0545

    PEP 8 and Docstring Example

--- Day24.py
+++ Day24.py
@@ -1,3 +1,4 @@
+import random
 #DocStrings
 #Python docstrings are the string literals that appear right after the defination of a function, method, class or module.
 
@@ -17,5 +18,29 @@ They are completely ingonred by the python interpreter.
 """
 # Python docstrings
 """ 
-As mentioned above, Python docstrings are string 
+As mentioned above, Python docstrings are strings used right after the defination of a function, method, class,
+or module (like in Example). They are used to document our code.
+we can access these docstring using the doc attribute.
 """
+#PEP 8
+""" PEP 8 is a document that provides gudlenes and best practises on how to write python code. It was written in 2001 by Guido van Rossum, Barry
+warsaw and Nick Coghlan. The primary focus of PEP 8 is to improve readability and consistency of python code.
+PEP stands for Python Enhancement Proposal , and there are several of them. A PEP is a document that describes a new 
+featurs proposed for python and documents aspects of python, like design and style, for the community."""
+
+def generate_joke():
+    """
+    This function generates a random joke from a predefined list.
+
+    Returns:
+        str: A random joke.
+    """
+    jokes = [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Why did the chicken go to the seance? To talk to the other side!",
+        "Why did the scarecrow win an award? Because he was outstanding in his field!"
+    ]
+    return random.choice(jokes)
+
+print(generate_joke())
+print(generate_joke.__doc__)
